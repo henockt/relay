2c3f9f97a02a545230a60a2a39667d6a8cae6807
Henock <contacthenockd@gmail.com>
2026-03-18 08:28:41 +0000

    add AutoMigrate

--- internal/store/db.go
+++ internal/store/db.go
@@ -4,10 +4,19 @@ import (
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 	"gorm.io/gorm/logger"
+
+	"github.com/henockt/relay/internal/models"
 )
 
 func NewDB(dsn string) (*gorm.DB, error) {
-	return gorm.Open(postgres.Open(dsn), &gorm.Config{
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{
 		Logger: logger.Default.LogMode(logger.Info),
 	})
+	if err != nil {
+		return nil, err
+	}
+	if err := db.AutoMigrate(&models.User{}, &models.Alias{}); err != nil {
+		return nil, err
+	}
+	return db, nil
 }
